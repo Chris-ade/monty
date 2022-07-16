3b5c2f5976ebd5b86b33cfe714851026d638a11b
Chris-ade <joeade12054@gmail.com>
2022-07-16 07:32:12 -0700

    Added main.c

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
 #include "monty.h"
+global_t global;
 
+/**
+ * main - Function for Monty
+ * @argc: num of inputs int
+ * @argv: code files char
+ * Return: None
+ */
+int main(int argc, char **argv )
+{
 
+}
