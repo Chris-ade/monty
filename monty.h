68e2e8d6254e591b70e202f0a1d81ae2284b4e1e
Chris-ade <joeade12054@gmail.com>
2022-07-17 15:37:08 -0700

    Update

--- monty.h
+++ monty.h
@@ -1,5 +1,9 @@
-#ifndef _MONTY_H_
-#define _MONTY_H_
+#define _POSIX_C_SOURCE  200809L
+#define _GNU_SOURCE
+
+#ifndef MONTY
+#define MONTY
+#define DELIM " \t\r\n"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,28 +22,46 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/**
+ * struct global_s - opcode and its function
+ * @num: the opcode
+ * @headstack: function to handle the opcode
+ * @line_number: ech line.
+ * @file: file.
+ * @gbuff: buff.
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct global_s
+{
+	char *num;
+	char *gbuff;
+	stack_t **headstack;
+	unsigned int line_number;
+	FILE *file;
+} global_t;
 
 extern global_t global;
 
@@ -60,6 +82,7 @@ stack_t *add_dnodeint_end(stack_t **head, const int n);
 int delete_node_index(stack_t **head, unsigned int index);
 void free_stack_t(stack_t **head);
 int _isnumber(char *s);
+void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
@@ -67,5 +90,5 @@ void _sub(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void exit_op(void);
-
 #endif
+
